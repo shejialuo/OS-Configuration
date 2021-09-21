2a5215b77d83d960f14734a63634b098bdaf2447
shejialuo <shejialuo@gmail.com>
2021-09-21 17:15:23 +0800

    Change the directory of system_config to make it easier.

---
+++ ArchLinux-i3/system_config/install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#* To install DM
+sudo pacman -S lightdm
+sudo systemctl enable lightdm
+sudo pacman -S lightdm-webkit2-greeter
+yay -S lightdm-webkit-theme-material-git
+
+#* To install network management
+sudo systemctl enable NetworkManager
+sudo pacman -S network-manager-applet
+
+#* To install sound management
+sudo pacman -S alsa-utils
+sudo pacman -S pulseaudio
+sudo pacman -S pavucontrol
+sudo pacman -S pamixer
+sudo pacman -S pulseaudio-bluetooth
+yay -S pa-applet-git
+
+#* To install system themes management
+sudo pacman -S lxappearance
+sudo pacman -S materia-gtk-theme
+sudo pacman -S papirus-icon-theme
+sudo pacman -S qt5ct
+yay -S qt5-styleplugins
+
+#* To install compositor
+sudo pacman -S picom
+
+#* To install font management
+sudo pacman -S ttf-sarasa-gothic
+sudo pacman -S ttf-fira-code
+sudo pacman -S ttf-font-awesome
+sudo pacman -S noto-fonts
+sudo pacman -S noto-fonts-cjk
+sudo pacman -S noto-fonts-emoji
+sudo pacman -S ttf-font-icons
+
+#* To install notification management
+sudo pacman -S dunst
+#* To configure dunst
+cp -r ./dunst ~/.config/
+
+#* To install bluetooth management
+sudo pacman -S bluez
+sudo pacman -S bluez-utils
+sudo pacman -S blueman
+
+#* To install brightness management
+sudo pacman -S xorg-xbacklight
+
+#* To install MS support
+sudo pacman -S ntfs-3g
+#* To install android mount support
+sudo pacman -S mtpfs
+yay -S jmtpfs
+#* To install iphone mount support
+yay -S ifuse-git
